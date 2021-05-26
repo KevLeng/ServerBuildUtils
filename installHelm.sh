9559848530facc2fe4ce07007061a3de536dd7cb
Kevin Leng <kevin.leng@dynatrace.com>
2021-05-26 11:22:21 +1000

    fixed versioning

--- installHelm.sh
+++ installHelm.sh
@@ -11,12 +11,12 @@ echo "Download and install helm"
 echo "-----------------------------------------------------------------------"
 echo "Installing Helm Version: $HELM_VERSION"
 
-wget https://get.helm.sh/helm-v${HELM_VERSION}-linux-amd64.tar.gz
-tar -zxvf helm-v${HELM_VERSION}-linux-amd64.tar.gz
+wget https://get.helm.sh/helm-${HELM_VERSION}-linux-amd64.tar.gz
+tar -zxvf helm-${HELM_VERSION}-linux-amd64.tar.gz
 sudo mv linux-amd64/helm /usr/local/bin/helm
 
 # remove downloaded files
-rm helm-v${HELM_VERSION}-linux-amd64.tar.gz
+rm helm-${HELM_VERSION}-linux-amd64.tar.gz
 rm -r linux-amd64
 
 echo "***********************************************************************"
