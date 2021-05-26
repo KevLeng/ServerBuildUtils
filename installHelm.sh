a3691d978209d3258e5fb81c4c2ab5501ec632e4
Kevin Leng <kevin.leng@dynatrace.com>
2021-05-26 11:20:25 +1000

    install helm initial commit

---
+++ installHelm.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Script to install Helm on ubuntu
+echo "***********************************************************************"
+echo "Started installHelm.sh"
+echo "***********************************************************************"
+
+export HELM_VERSION=${HELM_VERSION:-"v3.5.4"}
+
+echo "-----------------------------------------------------------------------"
+echo "Download and install helm"
+echo "-----------------------------------------------------------------------"
+echo "Installing Helm Version: $HELM_VERSION"
+
+wget https://get.helm.sh/helm-v${HELM_VERSION}-linux-amd64.tar.gz
+tar -zxvf helm-v${HELM_VERSION}-linux-amd64.tar.gz
+sudo mv linux-amd64/helm /usr/local/bin/helm
+
+# remove downloaded files
+rm helm-v${HELM_VERSION}-linux-amd64.tar.gz
+rm -r linux-amd64
+
+echo "***********************************************************************"
+echo "Finished installHelm.sh"
+echo "***********************************************************************"
