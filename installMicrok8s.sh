4966d03513ff301592c912d94393f7d089aee83a
Kevin Leng <kevin.leng@dynatrace.com>
2021-05-27 12:59:45 +1000

    refactor

--- installMicrok8s.sh
+++ installMicrok8s.sh
@@ -4,66 +4,50 @@ echo "***********************************************************************"
 echo "Started installMicrok8s.sh"
 echo "***********************************************************************"
 
-export MICROK8S_CHANNEL=${MICROK8S_CHANNEL:-"1.21/stable"}
+export MICROK8S_CHANNEL=${MICROK8S_CHANNEL:-"1.19/stable"}
+export USER=${USER:-"ubuntu"}
 
+# Wrapper for runnig commands for the real owner and not as root
+# thanks to keptn-in-a-box for this script which i have copied most of the commands from!
+
+alias bashas="sudo -H -u ${USER} bash -c"
+# Expand aliases for non-interactive shell
+shopt -s expand_aliases
+
+echo "Installing microk8s version: $MICROK8S_CHANNEL"
 sudo snap install microk8s --classic --channel=$MICROK8S_CHANNEL
 
 echo "allowing the execution of priviledge pods"
-echo "--allow-privileged=true" >> /var/snap/microk8s/current/args/kube-apiserver
+bash -c "echo \"--allow-privileged=true\" >> /var/snap/microk8s/current/args/kube-apiserver"
 
-sudo usermod -a -G microk8s $USER
+echo "Enable microk8s for user. User is $USER"
+usermod -a -G microk8s $USER
 
-iptables -P FORWARD ACCEPT
-ufw allow in on cni0 && sudo ufw allow out on cni0
-ufw default allow routed
+#iptables -P FORWARD ACCEPT
+#ufw allow in on cni0 && sudo ufw allow out on cni0
+#ufw default allow routed
 
+echo "Enable alias microk8s.kubectl kubectl"
 snap alias microk8s.kubectl kubectl
-
+echo "Add Snap to the system wide environment."
 sed -i 's~/usr/bin:~/usr/bin:/snap/bin:~g' /etc/environment
 
 homedirectory=$(eval echo ~$USER)
+echo "homedirectory is $homedirectory"
 
-mkdir $homedirectory/.kube
+bashas "mkdir $homedirectory/.kube"
+bashas "microk8s.config > $homedirectory/.kube/config"
 
-microk8s.config > $homedirectory/.kube/config
 
+echo "Start MicroK8S"
 microk8s.start
+echo "Enable MicroK8S DNS"
+microk8s.enable dns
+echo "Enable MicroK8S Storage"
+microk8s.enable storage
+echo "Enable MicroK8S Ingress"
+microk8s.enable ingress
 
 echo "***********************************************************************"
 echo "Finished installMicrok8s.sh"
 echo "***********************************************************************"
-
-
-microk8sInstall() {
-  if [ "$microk8s_install" = true ]; then
-    printInfoSection "Installing Microkubernetes with Kubernetes Version $MICROK8S_CHANNEL"
-    snap install microk8s --channel=$MICROK8S_CHANNEL --classic
-
-    printInfo "allowing the execution of priviledge pods "
-    bash -c "echo \"--allow-privileged=true\" >> /var/snap/microk8s/current/args/kube-apiserver"
-
-    printInfo "Add user $USER to microk8 usergroup"
-    usermod -a -G microk8s $USER
-
-    printInfo "Update IPTABLES, allow traffic for pods (internal and external) "
-    iptables -P FORWARD ACCEPT
-    ufw allow in on cni0 && sudo ufw allow out on cni0
-    ufw default allow routed
-
-    printInfo "Add alias to Kubectl (Bash completion for kubectl is already enabled in microk8s)"
-    snap alias microk8s.kubectl kubectl
-
-    printInfo "Add Snap to the system wide environment."
-    sed -i 's~/usr/bin:~/usr/bin:/snap/bin:~g' /etc/environment
-
-    printInfo "Create kubectl file for the user"
-    homedirectory=$(eval echo ~$USER)
-    bashas "mkdir $homedirectory/.kube"
-    bashas "microk8s.config > $homedirectory/.kube/config"
-  fi
-}
-
-microk8sStart() {
-  printInfoSection "Starting Microk8s"
-  bashas 'microk8s.start'
-}
