345bee20aabe8ab700ddc53c62595ce4cc5351e7
Kevin Leng <kevin.leng@dynatrace.com>
2021-06-18 10:48:19 +1000

    added installDocker script

---
+++ installDocker.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Script to install Helm on ubuntu
+echo "***********************************************************************"
+echo "Started installDocker.sh"
+echo "***********************************************************************"
+
+export INSTALL_DOCKER=${INSTALL_DOCKER:-true}
+export INSTALL_DOCKER_COMPOSE=${INSTALL_DOCKER_COMPOSE:-true}
+
+
+if [[ "$INSTALL_DOCKER" == "true" ]]; then
+    echo "Install Docker"
+    apt install docker.io -y
+fi
+
+if [[ "$INSTALL_DOCKER_COMPOSE" == "true" ]]; then
+    echo "Install Docker Compose"
+    apt install docker-compose
+fi
+
+
+echo "***********************************************************************"
+echo "Finished installDocker.sh"
+echo "***********************************************************************"
