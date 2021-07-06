28c72dfbadbc98b549113c071dd5b7cb98b31245
Kevin Leng <kevin.leng@dynatrace.com>
2021-07-06 11:26:26 +1000

    initial commit

---
+++ installSockshop.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Script to install sockshop on k8s
+echo "-----------------------------------------------------------------------"
+echo "Install Sockshop"
+echo "-----------------------------------------------------------------------"
+
+SOCKSHOP_REPO="https://github.com/KevLeng/sockshop"
+
+git clone $SOCKSHOP_REPO
+cd sockshop
+
+./deploy-sockshop.sh
+
+echo "Sockshop deployed."
+export INGRESSCLASS=nginx
+./deploy-ingress.sh
+
+echo "Sockshop ingress deployed."
