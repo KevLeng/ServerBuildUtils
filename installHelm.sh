9aa1e33dd63834557737eba085bb6cc552af62c4
Kevin Leng <kevin.leng@dynatrace.com>
2021-07-16 19:14:39 +1000

    updated to helm package instaler

--- installHelm.sh
+++ installHelm.sh
@@ -11,13 +11,19 @@ echo "Download and install helm"
 echo "-----------------------------------------------------------------------"
 echo "Installing Helm Version: $HELM_VERSION"
 
-wget https://get.helm.sh/helm-${HELM_VERSION}-linux-amd64.tar.gz
-tar -zxvf helm-${HELM_VERSION}-linux-amd64.tar.gz
-sudo mv linux-amd64/helm /usr/local/bin/helm
+#wget https://get.helm.sh/helm-${HELM_VERSION}-linux-amd64.tar.gz
+#tar -zxvf helm-${HELM_VERSION}-linux-amd64.tar.gz
+#sudo mv linux-amd64/helm /usr/local/bin/helm
 
 # remove downloaded files
-rm helm-${HELM_VERSION}-linux-amd64.tar.gz
-rm -r linux-amd64
+#rm helm-${HELM_VERSION}-linux-amd64.tar.gz
+#rm -r linux-amd64
+
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+
+rm get_helm.sh
 
 echo "***********************************************************************"
 echo "Finished installHelm.sh"
